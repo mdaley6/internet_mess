1b88a522559d8f346d746dc9121164563d7e13c7
Michael Daley <daleyhockey9@gmail.com>
2023-03-27 18:03:40 -0400

    some work on the golf card games parameters

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Internet Mess
-A repository that contains a hot mess of a website
-I made it to build test &amp; build whatever whatever I feel like stress freeeeee
+A repository that contains a hot mess of a we
+bsite. I created said mess as a sandbox to build/test/try whatever I feel like without expectations
 
 **In Progress Features**
 - Trivia Game
--- nextjs-blog/pages/golfcard.js
+++ nextjs-blog/pages/golfcard.js
@@ -29,9 +29,31 @@ export default function GolfCards() {
         <NavBar />
         <div className='bg-blue-300'>
             <h1 className='text-3xl'>Golf Card Generator</h1>
-            <p>Set Parameters</p>
-            <p>parameter 1</p>
-            <p>parameter 2</p>
+            <p>Parameters</p>
+            <p>Players</p>
+            <select name="number">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+            </select>
+            <label for="team-game">Team Game:</label>
+            <input type="radio" id="team-game-yes" name="team-game" value="yes"></input>
+            <label for="team-game-yes">Yes</label>
+            <input type="radio" id="team-game-no" name="team-game" value="no" checked></input>
+            <label for="team-game-no">No</label>
+
+            <label for="game-length">Game Length:</label>
+            <select id="game-length" name="game-length">
+                <option value="1">1</option>
+                <option value="3">3</option>
+                <option value="9">9</option>
+                <option value="18">18</option>
+            </select>
             <button className='border-2 border-yellow-400 hover:border-yellow-600 bg-green-200 rounded m-2 p-1 hover:bg-green-500' onClick={generateCard}>Generate Card</button>
             {currentCard}
         </div>
